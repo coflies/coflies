3e84244d5a337c87996556f770287d93bab9c5dc
To Nguyen Thanh Vu <tntvu@TMA.COM.VN>
2018-09-18 15:58:27 +0700

    add todo

--- coflies.go
+++ coflies.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// TODO agruments parsing
+	// TODO project initialize
+	// TODO compile if necessary
+	// TODO run test suites
+	// TODO handling output and return
 	printUsage()
 }
 
